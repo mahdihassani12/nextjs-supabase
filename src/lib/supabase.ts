6aaad0e0bd72516dbff2fcf3d4971fdc59009c64
Mahdi Hassai <hassanim430@gmail.com>
2024-01-28 21:39:01 +0430

    updating middleware

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,11 +1,3 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
-export const supabase = createClient(
-  process.env.NEXT_PUBLIC_URL!,
-  process.env.NEXT_PUBLIC_ANON_KEY!
-);
-
-export const supabaseAdmin = createClient(
-  process.env.NEXT_PUBLIC_URL!,
-  process.env.NEXT_PUBLIC_SERVICE_ROLE_KEY!
-);
+export const supabase = createClientComponentClient();
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,11 @@
 import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
 import { NextRequest, NextResponse } from "next/server";
+const supabaseUrl = process.env.NEXT_PUBLIC_URL; 
+const supabaseKey = process.env.NEXT_PUBLIC_ANON_KEY; 
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
-  const supabase = createMiddlewareClient({ req, res });
+  const supabase = createMiddlewareClient({ req, res }, {supabaseUrl, supabaseKey});
   const {
     data: { session },
   } = await supabase.auth.getSession();
